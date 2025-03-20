188ab1158e5525cdbb82ac4d0131aae0b21f48d8
Saidabror289 <azizovsaidabror123@gmail.com>
2025-03-20 14:23:56 +0500

    saidabror3 yangilik

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Home from './pages/home'
 import About from './pages/about'
 import './App.css'
+import Test from './pages/test'
 
 function App() {
   return (
     <>
     <Home/>
     <About/>
+    <Test/>
     </>
   )
 }
---
+++ src/pages/test.jsx
@@ -0,0 +1,24 @@
+import './test.css'
+function Test(){
+    return(
+        <div className="test">
+<h4>TESTOMINALS</h4>
+<h1>Client says about us</h1>
+<div className="container">
+    <div className="box">
+<h4>Louise Saville</h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+elit, sed do eiusmod tempor incididunt ut labore
+et dolore magna aliqua.</p>
+    </div>
+    <div className="box">
+<h4>Rekha Varadwaz </h4>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing 
+elit, sed do eiusmod tempor incididunt ut labore
+et dolore magna aliqua.</p>
+    </div>
+</div>
+        </div>
+    )
+}
+export default Test
